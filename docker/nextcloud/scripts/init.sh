74aa7a3f2113c6eb3325c1da5fe99a4848e253a1
AkashiSN <btorntireinvynriy@gmail.com>
2023-05-15 22:51:54 +0900

    add nextcloud

---
+++ docker/nextcloud/scripts/init.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -eu
+
+chmod 0770 "${NEXTCLOUD_DATA_DIR}"
+
+set +u
+if [ -n "${EXTERNAL_DIR}" ]; then
+  chown www-data:www-data "${EXTERNAL_DIR}"
+  chmod 0770 "${EXTERNAL_DIR}"
+fi
+set -u
+
+exec "$@"
